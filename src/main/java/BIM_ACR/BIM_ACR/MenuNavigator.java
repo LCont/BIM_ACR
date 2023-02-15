992b812ded4be929ebc355bc9eb512b1080518a5
LCont <soycontreras@gmail.com>
2023-02-14 23:23:41 -0600

    navegacion

--- src/main/java/BIM_ACR/BIM_ACR/MenuNavigator.java
+++ src/main/java/BIM_ACR/BIM_ACR/MenuNavigator.java
@@ -3,6 +3,7 @@ import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,13 +17,19 @@ public class MenuNavigator {
         
     }
     
-    public static void abriracr(){
-    	System.setProperty("webdriver.chrome.driver", "D:\\Proyectos\\BIM_ACR\\chromedriver.exe");
+    public static void abriracr() {
+    	final String dir = System.getProperty("user.dir");
+		char currentChar = dir.charAt(1);
+		if (currentChar == ':') {
+			System.setProperty("webdriver.chrome.driver", "D:\\Proyectos\\BIM_ACR\\chromedriver.exe");
+		}else {
+			System.setProperty("webdriver.chrome.driver", "/avaya/app/chromedriver");
+		}
+    	
         WebDriver driver = new ChromeDriver();
         try {
             driver.get("https://fabc.ddns.net:7070");
-            //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-            
+            //LOGiN
             WebDriverWait w = new WebDriverWait(driver,Duration.ofSeconds(20));
             w.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("#firstName-input")));
             System.out.println("Title of the page is -> " + driver.getTitle());
@@ -32,13 +39,97 @@ public class MenuNavigator {
             password.sendKeys("FABCadm22!");
             WebElement boton = driver.findElement(By.cssSelector(".gwt-Button"));
             boton.click();
-            w.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("menu_primary_applications")));
-            WebElement navegar = driver.findElement(By.id("menu_primary_applications"));
+            
+            //NAVEGAR
+            // menu aplicaciones
+            String xpath = new String();
             Actions action = new Actions(driver);
+            
+            xpath="//div[@id='menu_primary_applications']";
+            //w.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("menu_primary_applications")));
+            w.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(xpath)));		
+            WebElement navegar = driver.findElement(By.id("menu_primary_applications"));
+            System.out.println("Navegar -> " + navegar.getText());
             action.moveToElement(navegar).perform();
-            navegar = driver.findElement(By.id("x-menu-el-menu_item_media_manager"));
+            
+            //menu aplicacion, opci[on media manager
+            xpath="//div[@id='menu_item_media_manager']/span";
+            w.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(xpath)));
+            
+            //navegar = driver.findElement(By.xpath(xpath));
+            navegar = driver.findElement(By.id("menu_item_media_manager"));
+            System.out.println("Navegar -> " + navegar.getText());
+            //action.moveToElement(navegar).perform();
             navegar.click();
-            		
+            
+            //dropbox del media manager
+            xpath="//input[@id='Media_Mgr_Menu-input']";
+            w.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(xpath)));
+            navegar = driver.findElement(By.xpath(xpath));
+            navegar.sendKeys(Keys.ARROW_DOWN);
+            xpath="/html/body/div[6]/div/div[4]";
+            w.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(xpath)));
+            navegar = driver.findElement(By.xpath(xpath));
+            navegar.click();
+            
+            //FILTROS
+
+            //FILTROS -Hora
+            xpath="//div[@id='CMM_RecStartTimeField_Id']/div/input";
+            w.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(xpath)));
+            driver.findElement(By.xpath(xpath)).click();
+            //navegar.click();
+            int i=0;
+            while(i<(8*4)) {
+            	navegar = driver.findElement(By.xpath(xpath));
+            	navegar.sendKeys(Keys.ARROW_DOWN);
+            	i++;
+            }
+            navegar = driver.findElement(By.xpath(xpath));
+            navegar.sendKeys(Keys.ENTER);
+            
+            xpath="//div[@id='CMM_RecEndTimeField_Id']/div/input";
+            w.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(xpath)));
+            navegar = driver.findElement(By.xpath(xpath));
+            navegar.click();
+            i=0;
+            while(i<(20*4)) {
+            	navegar = driver.findElement(By.xpath(xpath));
+            	navegar.sendKeys(Keys.ARROW_DOWN);
+            	i++;
+            }
+            navegar = driver.findElement(By.xpath(xpath));
+            navegar.sendKeys(Keys.ENTER);
+            
+            
+            //FILTROS -FECHA
+            xpath="//input[@id='x-widget-308-input']";
+            w.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(xpath)));
+            driver.findElement(By.xpath(xpath)).click();
+            w.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("GNOAMXJCDNB")));
+            navegar = driver.findElement(By.className("GNOAMXJCDNB"));
+            navegar.click();
+            
+            xpath="//input[@id='x-widget-310-input']";
+            w.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(xpath)));
+            driver.findElement(By.xpath(xpath)).click();
+            w.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("GNOAMXJCDNB")));
+            navegar = driver.findElement(By.className("GNOAMXJCDNB"));
+            navegar.click();
+            
+            
+        
+            
+            //FILTROS - FILTRAR
+            xpath="//button[@id='CMM_RecButtonApplyCriteria_Id']";
+            w.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(xpath)));
+            navegar = driver.findElement(By.xpath(xpath));
+            navegar.click();
+         
+            
+            
+            
+            
         } finally {
             //driver.quit();
         }
